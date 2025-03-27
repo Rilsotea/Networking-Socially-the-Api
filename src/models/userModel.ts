8980edf56fab36158931ea0d63329aff4d357d9a
Zach <zowen17@gmail.com>
2025-03-27 03:59:04 -0400

    removed unique line of code for usermodel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -13,13 +13,11 @@ const userSchema = new Schema<IUser>(
         username: {
             type: String,
             required: true,
-            unique: true,
             trim: true,
         },
         email: {
             type: String,
             required: true,
-            unique: true,
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
         },
 
