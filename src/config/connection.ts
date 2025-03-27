7e92aeec691abb7fd5064ac07b17e14e4a30b418
Zach <zowen17@gmail.com>
2025-03-26 22:22:26 -0400

    added code to connectionsts

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -5,4 +5,20 @@ dotenv.config();
 
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/networkingsocially';
 
+// Connect to MongoDB
+mongoose.connect(connectionString, {});
+
+// Event listeners for connection
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected to the database');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose is disconnected from the database');
+});
+
 export default mongoose.connection;
