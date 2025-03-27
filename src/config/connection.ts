574a53229816cf2b2f4ac3a7655048dfb44bee2b
Zach <zowen17@gmail.com>
2025-03-26 22:16:27 -0400

    adjusted typescript patch and fixed config as well

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -5,13 +5,4 @@ dotenv.config();
 
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/networkingsocially';
 
-mongoose.connect(connectionString, {
-})
-    .then(() => {
-        console.log('MongoDB connected successfully');
-    })
-    .catch(err => {
-        console.error('MongoDB connection error:', err);
-    });
-
 export default mongoose.connection;
--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -19,7 +19,7 @@ router.route('/:thoughtId').get(getThoughtById).delete(deleteThought);
 // /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(addReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
+// /api/thoughts/:thoughtId/reactions/:reactionId 
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 export { router as thoughtRouter };
